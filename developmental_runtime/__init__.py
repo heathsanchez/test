ab3b8f201b80b6b72e5a19a2f6a691e2d536e9dd
Heath Sanchez <32909488+heathsanchez@users.noreply.github.com>
2026-08-22 08:27:51 +1200

    V30: export runtime engine

--- developmental_runtime/__init__.py
+++ developmental_runtime/__init__.py
@@ -3,10 +3,11 @@ from .intervention import Intervention, InterventionKind, ObligationEvidence, Te
 from .commitment import Route, RoutingDecision, common_interventions, optimal_experiment_policy, route
 from .synthesis import SynthesisRegistry
 from .lawbook import Lawbook, RetainedLaw
+from .engine import DevelopmentalRuntime, EngineEvent
 
 __all__ = [
     "DevelopmentalState", "Intervention", "InterventionKind", "ObligationEvidence",
     "Terminal", "TransitionRecord", "lawful", "Route", "RoutingDecision",
     "common_interventions", "optimal_experiment_policy", "route", "SynthesisRegistry",
-    "Lawbook", "RetainedLaw",
+    "Lawbook", "RetainedLaw", "DevelopmentalRuntime", "EngineEvent",
 ]
