aace5bbdb2d193c19738cbdfb52880863030a860
Heath Sanchez <32909488+heathsanchez@users.noreply.github.com>
2026-09-26 11:39:03 +1200

    Build shortcut Collatz formalization

--- lakefile.lean
+++ lakefile.lean
@@ -7,4 +7,4 @@ package collatzFinal where
 @[default_target]
 lean_lib CollatzFinal where
   srcDir := "formal"
-  roots := #[`Collatz.FinalKernel, `Collatz.Certificate]
+  roots := #[`Collatz.FinalKernel, `Collatz.Certificate, `Collatz.Shortcut]
