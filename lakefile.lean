1acbee1421eb543de862ba37934c45649c6a4c95
Heath Sanchez <32909488+heathsanchez@users.noreply.github.com>
2026-09-26 11:33:43 +1200

    Build Collatz finite certificate checker

--- lakefile.lean
+++ lakefile.lean
@@ -7,4 +7,4 @@ package collatzFinal where
 @[default_target]
 lean_lib CollatzFinal where
   srcDir := "formal"
-  roots := #[`Collatz.FinalKernel]
+  roots := #[`Collatz.FinalKernel, `Collatz.Certificate]
