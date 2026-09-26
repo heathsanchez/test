cb6bbe047b7005b67dacd6989634d5a3946dbc3c
Heath Sanchez <32909488+heathsanchez@users.noreply.github.com>
2026-09-26 16:47:45 +1200

    fix(nucleus): rebuild neutral trace from qualified base

--- metatron-kernel/src/convert.rs
+++ metatron-kernel/src/convert.rs
@@ -723,19 +723,9 @@ fn compare_neutral_heads(
             }
             Judgment::proven((), "same-rigid-constant")
         }
-        _ => {
-            if std::env::var_os("NUCLEUS_TRACE_CONVERSION").is_some() {
-                eprintln!(
-                    "NUCLEUS_CONV_HEAD_MISMATCH left={:?} right={:?} left_spine={} right_spine={}",
-                    left.head,
-                    right.head,
-                    left.spine.len(),
-                    right.spine.len()
-                );
-            }
-            Judgment::refuted("distinct-neutral-heads")
-        }
+        _ => {\n            if std::env::var_os("NUCLEUS_TRACE_CONVERSION").is_some() {\n                eprintln!("NUCLEUS_CONV_HEAD_MISMATCH left={:?} right={:?} left_spine={} right_spine={}", left.head, right.head, left.spine.len(), right.spine.len());\n            }\n            Judgment::refuted("distinct-neutral-heads")\n        }
     }
+}
 
 fn value_as_type(value: &Value, depth: usize) -> Option<TypeValue> {
     match value {
